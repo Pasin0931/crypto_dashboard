a7a874ee7f985450cd5806761359fa23961df31c
Pasin <pasin0931@gmail.com>
2025-11-27 14:34:38 +0700

    Add skeliton ui v1

--- libs/widget_lib.py
+++ libs/widget_lib.py
@@ -9,7 +9,7 @@ class System:
         root = tk.Tk()
         root.config(bg="#252526")
         root.title("My Crypto")
-        root.geometry("1000x600")
+        root.geometry("1070x578") # resize window
         return root
     
 class Widget:
@@ -25,17 +25,22 @@ class Label(Widget):
     def __init__(self, root):
         super().__init__(root)
     
-    def create_label(self, msg):
+    def create_label(self, msg, s, st):
         this_label = ttk.Label(self.root, text=msg)
+        this_label.config(style='Big.TLabel', font=('Helvetica', s, st)) # normal/bold
+        this_label.config(background="#252526", foreground="white")
         self.all_widgets.append(this_label)
         return this_label
     
 class Button(Widget):
     def __init__(self, root):
         super().__init__(root)
-        
+
     def create_button(self, msg, comm):
         this_button = ttk.Button(self.root, text=msg, command=comm)
+        this_button.config(style="Crypto.TButton")
+        style = ttk.Style()
+        style.configure("Crypto.TButton", background="#1E1E1E", foreground="#000000")
         self.all_widgets.append(this_button)
         return this_button
     
@@ -45,5 +50,11 @@ class Frame(Widget):
             
     def create_frame(self, r, bw, pd, w, h):
         this_frame = ttk.Frame(relief=r, borderwidth=bw, padding=pd, width=w, height=h)
+        this_frame.config(style="Crypto.TFrame")
+        style = ttk.Style()
+        style.configure("Crypto.TFrame", background="#1E1E1E")
         self.all_widgets.append(this_frame)
         return this_frame
+    
+    
+    
--- main.py
+++ main.py
@@ -11,17 +11,19 @@ label = Label(this_root)
 button = Button(this_root)
 frame = Frame(this_root)
 
-label_1 = label.create_label("Hello world")
-button_1 = button.create_button("button", None)
-frame_1 = frame.create_frame("ridge", 10, None, 700, 400)
+header_ = label.create_label("BTC/UTC Dashboard", 20, "bold")
+sub_header = label.create_label("This dashboard shows the value of bitcoin value", 12, "normal")
+b1 = button.create_button("Menu", None)
+b2 = button.create_button("Test1", None)
+b3 = button.create_button("Test2", None)
+frame_1 = frame.create_frame("ridge", 10, None, 650, 400)
+close_button = button.create_button("close", comm=this_root.destroy)
 
-close_button = button.create_button("close", None)
-
-label_1.pack()
-button_1.pack()
-frame_1.pack(padx=10, pady=10)
-
-close_button.pack()
+header_.pack(anchor="e", padx=55, pady=(25, 0)) # Header
+sub_header.pack(anchor="e", padx=55, pady=(0, 20))
+# button_1.pack()
+frame_1.pack(anchor='e', padx=30)
+close_button.pack(anchor='se', pady=(25, 0), padx=40)
 
 if __name__ == "__main__":
     this_root.mainloop()
